582d2a1643563fdbf499cecc0ea33e7724e10054
Susan Pan <susan.pan@octopus.com>
2024-05-08 11:10:08 +1000

    cleanup tests, add cases

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -7,46 +7,40 @@ namespace GildedRoseTests;
 public class GildedRoseTest
 {
     [Test]
-    public void Foo()
-    {
-        var items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 10 } };
-        
-        var app = new GildedRose(items);
-        app.UpdateQuality();
-        
-        Assert.That(items[0].Name, Is.EqualTo("foo"));
-        Assert.That(items[0].SellIn, Is.EqualTo(9));
-        Assert.That(items[0].Quality, Is.EqualTo(9));
-    }
-
-    [Test]
-    public void QualityIsNever_Negative()
+    [TestCase(10, 0)]
+    [TestCase(-10, 0)]
+    public void QualityIsNever_Negative(int sellIn, int quality)
     {
         var itemName = "anotheritem-12928";
-        var items = new List<Item> { new Item { Name = itemName, SellIn = 0, Quality = 0 } };
+        var items = new List<Item> { new Item { Name = itemName, SellIn = sellIn, Quality = quality } };
         
         var app = new GildedRose(items);
         app.UpdateQuality();
 
         var testItem = items[0];
         Assert.That(testItem.Name, Is.EqualTo(itemName));
-        Assert.That(testItem.SellIn, Is.EqualTo(-1));
-        Assert.That(testItem.Quality, Is.GreaterThanOrEqualTo(0));
+        Assert.That(testItem.SellIn, Is.EqualTo(sellIn - 1));
+        Assert.That(testItem.Quality, Is.EqualTo(0));
     }
     
-    [Test]
-    public void QualityIsNever_AboveFifty()
+    [Theory]
+    [TestCase("Aged Brie",10, 50)]
+    [TestCase("Aged Brie", -2, 49)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 1, 48)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 5, 49)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 10, 50)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 15, 50)]
+    public void QualityIsNever_AboveFifty(string itemName, int sellIn, int quality)
     {
-        var itemName = "Aged Brie";
-        var items = new List<Item> { new Item { Name = itemName, SellIn = 10, Quality = 50 } };
+        var items = new List<Item> { new Item { Name = itemName, SellIn = sellIn, Quality = quality } };
         
         var app = new GildedRose(items);
         app.UpdateQuality();
 
         var testItem = items[0];
         Assert.That(testItem.Name, Is.EqualTo(itemName));
-        Assert.That(testItem.SellIn, Is.EqualTo(9));
-        Assert.That(testItem.Quality, Is.GreaterThanOrEqualTo(50));
+        Assert.That(testItem.SellIn, Is.EqualTo(sellIn - 1));
+        Assert.That(testItem.Quality, Is.EqualTo(50)); 
     }
     
     [Test]
@@ -61,7 +55,7 @@ public class GildedRoseTest
         var testItem = items[0];
         Assert.That(testItem.Name, Is.EqualTo(itemName));
         Assert.That(testItem.SellIn, Is.EqualTo(9));
-        Assert.That(testItem.Quality, Is.GreaterThanOrEqualTo(9));
+        Assert.That(testItem.Quality, Is.EqualTo(9));
     }
     
     [Test]
@@ -149,64 +143,78 @@ public class GildedRoseTest
         Assert.That(testItem.SellIn, Is.EqualTo(10));
     }
     
-    [Test]
-    public void BackstagePassQualityIncreases_BeforeSellDate()
+    [Theory]
+    [TestCase(50, 49, 50)]
+    [TestCase(11, 50, 50)]
+    [TestCase(12, 0, 1)]
+    public void BackstagePassQualityIncreases_BeforeSellDate(int sellIn, int quality, int expectedQuality)
     {
         var itemName = "Backstage passes to a TAFKAL80ETC concert";
-        var items = new List<Item> { new Item { Name = itemName, SellIn = 15, Quality = 10 } };
+        var items = new List<Item> { new Item { Name = itemName, SellIn = sellIn, Quality = quality } };
         
         var app = new GildedRose(items);
         app.UpdateQuality();
 
         var testItem = items[0];
         Assert.That(testItem.Name, Is.EqualTo(itemName));
-        Assert.That(testItem.SellIn, Is.EqualTo(14));
-        Assert.That(testItem.Quality, Is.EqualTo(11));
+        Assert.That(testItem.SellIn, Is.EqualTo(sellIn - 1));
+        Assert.That(testItem.Quality, Is.EqualTo(expectedQuality));
     }
     
-    [Test]
-    public void BackstagePassQualityIncreasesDouble_WithinTenDaysOfSellDate()
+    [Theory]
+    [TestCase(10, 48, 50)]
+    [TestCase(10, 49, 50)]
+    [TestCase(6, 0, 2)]
+    public void BackstagePassQualityIncreasesDouble_WithinTenDaysOfSellDate(int sellIn, int quality, int expectedQuality)
     {
         var itemName = "Backstage passes to a TAFKAL80ETC concert";
-        var items = new List<Item> { new Item { Name = itemName, SellIn = 10, Quality = 10 } };
+        var items = new List<Item> { new Item { Name = itemName, SellIn = sellIn, Quality = quality } };
         
         var app = new GildedRose(items);
         app.UpdateQuality();
 
         var testItem = items[0];
         Assert.That(testItem.Name, Is.EqualTo(itemName));
-        Assert.That(testItem.SellIn, Is.EqualTo(9));
-        Assert.That(testItem.Quality, Is.EqualTo(12));
+        Assert.That(testItem.SellIn, Is.EqualTo(sellIn - 1));
+        Assert.That(testItem.Quality, Is.EqualTo(expectedQuality));
     }
     
-    [Test]
-    public void BackstagePassQualityIncreasesTriple_WithinFiveDaysOfSellDate()
+    [Theory]
+    [TestCase(5, 50, 50)]
+    [TestCase(5, 49, 50)]
+    [TestCase(5, 47, 50)]
+    [TestCase(1, 49, 50)]
+    [TestCase(1, 0, 3)]
+    public void BackstagePassQualityIncreasesTriple_WithinFiveDaysOfSellDate(int sellIn, int quality, int expectedQuality)
     {
         var itemName = "Backstage passes to a TAFKAL80ETC concert";
-        var items = new List<Item> { new Item { Name = itemName, SellIn = 5, Quality = 10 } };
+        var items = new List<Item> { new Item { Name = itemName, SellIn = sellIn, Quality = quality } };
         
         var app = new GildedRose(items);
         app.UpdateQuality();
 
         var testItem = items[0];
         Assert.That(testItem.Name, Is.EqualTo(itemName));
-        Assert.That(testItem.SellIn, Is.EqualTo(4));
-        Assert.That(testItem.Quality, Is.EqualTo(13));
+        Assert.That(testItem.SellIn, Is.EqualTo(sellIn - 1));
+        Assert.That(testItem.Quality, Is.EqualTo(expectedQuality));
     }
     
-        
-    [Test]
-    public void BackstagePassQualityBecomesZero_PastSellDate()
+    [Theory]
+    [TestCase(0, 50, 0)]
+    [TestCase(-5, 49, 0)]
+    [TestCase(0, -50, 0)]
+    [TestCase(-10, -1, 0)]
+    public void BackstagePassQualityBecomesZero_PastSellDate(int sellIn, int quality, int expectedQuality)
     {
         var itemName = "Backstage passes to a TAFKAL80ETC concert";
-        var items = new List<Item> { new Item { Name = itemName, SellIn = 0, Quality = 10 } };
+        var items = new List<Item> { new Item { Name = itemName, SellIn = sellIn, Quality = quality } };
         
         var app = new GildedRose(items);
         app.UpdateQuality();
 
         var testItem = items[0];
         Assert.That(testItem.Name, Is.EqualTo(itemName));
-        Assert.That(testItem.SellIn, Is.EqualTo(-1));
-        Assert.That(testItem.Quality, Is.EqualTo(0));
+        Assert.That(testItem.SellIn, Is.EqualTo(sellIn - 1));
+        Assert.That(testItem.Quality, Is.EqualTo(expectedQuality));
     }
 } 
